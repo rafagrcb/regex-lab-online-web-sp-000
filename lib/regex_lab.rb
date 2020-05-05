a9282f9f068ac25f46f9b5f82d2c79c8374b8b2b
Rafael Garcia <rafagrcb@gmail.com>
2020-05-05 21:09:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text =~ /^[un].*ing$/ != false
+  if text =~ /^[un].*ing$/
 end
 
 def words_five_letters_long(text)
-  if text =~ /\b\w{1,5}\b/ != false
+  if text =~ /\b\w{1,5}\b/ 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
