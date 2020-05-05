f1e52b765de5f18fc555251d7e95e441b7e1fbb6
Rafael Garcia <rafagrcb@gmail.com>
2020-05-05 21:09:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,10 +4,12 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   if text =~ /^[un].*ing$/
+  end
 end
 
 def words_five_letters_long(text)
-  if text =~ /\b\w{1,5}\b/ 
+  if text =~ /\b\w{1,5}\b/
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
