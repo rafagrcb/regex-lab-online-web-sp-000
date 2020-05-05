1c98c69c7eeaceb31d129f4ec4a5a7b5d65603a0
Rafael Garcia <rafagrcb@gmail.com>
2020-05-05 21:05:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if (text =~ /^[un].*ing$/)
+  if text =~ /^[un].*ing$/
     return true
   else
     return false
